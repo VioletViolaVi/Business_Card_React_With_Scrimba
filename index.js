5412801a1f194a1a0fed1e9dafd8b76de9ef604c
vivian <vivian.v18723@gmail.com>
2021-12-12 17:11:04 +0000

    Render list to browser with react.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 // 1st: what do you want to render? 2nd: where do you want to render it?
-ReactDOM.render(<h1>Hello everyone!</h1>, document.getElementById("root"));
+ReactDOM.render(
+  <div>
+    <h1>Teen Titans</h1>
+    <p>The team members are:</p>
+    <ul>
+      <li>Starfire</li>
+      <li>Cyborg</li>
+      <li>Raven</li>
+      <li>BeastBoy</li>
+      <li>Robin</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
